c0025f6ec0d0ff7bbc551146abb7da9498707267
Viktor <vitostagan@mail.ru>
2020-04-29 15:46:31 +0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/mop/learnprogrammingapp/MainFragment.java
+++ app/src/main/java/com/mop/learnprogrammingapp/MainFragment.java
@@ -14,12 +14,16 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+<<<<<<< Updated upstream
 <<<<<<<
 import android.widget.ImageButton;
 import android.widget.ImageView;
 =======
 import android.widget.ImageView;
 >>>>>>>
+=======
+import android.widget.ImageView;
+>>>>>>> Stashed changes
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
